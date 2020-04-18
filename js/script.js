8765c77cb2454f0bcafbf405b8e0536ee42a86c2
Jarosław Latek <jareklatek@gmail.com>
2020-04-18 22:01:52 +0200

    Add function that calculate petrol costs. Little styling of text elements and make it works properly

--- js/script.js
+++ js/script.js
@@ -1,19 +1,55 @@
-var rangeSlider = document.getElementById("city");
-var rangeSliderLong = document.getElementById("long-distance");
-var rangeBullet = document.getElementById("rs-bullet");
-var rangeBulletLong = document.getElementById("rs-bullet-long");
+const cityTrack = document.getElementById("city");
+const abroadTrack = document.getElementById("abroad");
+const fuel = {
+  price: 4.48,
+  combustion: {
+    abroad: 5.5,
+    city: 6.2
+    },
+  hybrid: {
+    abroad: 8.4,
+    city: 1.2
+    }
+  }
+let prices = {
+  combustion: '',
+  hybrid: ''
+}
+
+let rangeSlider = document.getElementById("city");
+let rangeSliderLong = document.getElementById("abroad");
+let rangeBullet = document.getElementById("rs-bullet");
+let rangeBulletLong = document.getElementById("rs-bullet-long");
+let yourResult = document.getElementById("result");
 
 rangeSlider.addEventListener("input", showSliderValue, false);
 rangeSliderLong.addEventListener("input", showSliderLongValue, false);
 
 function showSliderValue() {
   rangeBullet.innerHTML = rangeSlider.value;
-  var bulletPosition = (rangeSlider.value /rangeSlider.max);
-  rangeBullet.style.left = (bulletPosition * 578) + "px";
+  let bulletPosition = (rangeSlider.value /rangeSlider.max);
+  rangeBullet.style.left = (bulletPosition * 780) + "px";
+  calculateFuelPrice();
 }
 
 function showSliderLongValue() {
-    rangeBullet.innerHTML = rangeSliderLong.value;
-    var bulletPosition = (rangeSliderLong.value /rangeSliderLong.max);
-    rangeBullet.style.left = (bulletPosition * 578) + "px";
+    rangeBulletLong.innerHTML = rangeSliderLong.value;
+    let bulletPosition = (rangeSliderLong.value /rangeSliderLong.max);
+    rangeBulletLong.style.left = (bulletPosition * 780) + "px";
+    calculateFuelPrice();
+}
+
+function calculateFuelPrice() {
+  prices.combustion = (cityTrack.value / 100 * fuel.combustion.city) + (abroadTrack.value / 100 * fuel.combustion.abroad) * fuel.price;
+  prices.hybrid = (cityTrack.value / 100 * fuel.hybrid.city) + (abroadTrack.value / 100 * fuel.hybrid.abroad) * fuel.price;
+  if (prices.combustion > prices.hybrid) {
+    yourResult.innerHTML = 'Petrol costs tell us that you should buy a hybrid.';
+    // console.log('Petrol costs tell us that you should buy a hybrid.');  
+  } else {
+    yourResult.innerHTML = 'You are a speedracer. Petrol car is your destiny.';
+    // console.log('You are a speedracer. Petrol car is your destiny.');
   }
+  // console.log('Silnik spalinowy', prices.combustion, 'Silnik hybrydowy', prices.hybrid);
+  // console.log('Różnica w cenie wynosi', prices.combustion - prices.hybrid);
+}
+
